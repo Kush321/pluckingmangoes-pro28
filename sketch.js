d21d492d49103c725621c0272e90c689690b9067
Kush321 <78313670+Kush321@users.noreply.github.com>
2021-03-17 08:59:43 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,83 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint=Matter.Constraint;
+var treeObj, stoneObj,groundObject, launcherObject;
+var mango1;
+var world,boy;
+
+function preload(){
+	boy=loadImage("images/boy.png");
+  }
+
+function setup() {
+	createCanvas(1300, 600);
+	engine = Engine.create();
+	world = engine.world;
+
+	mango1=new mango(1100,100,30);
+	mango2=new mango(1000,120,35);
+	mango3=new mango(1200,135,35);
+	mango4=new mango(1110,30,25);
+	mango5=new mango(1150,200,30);
+	mango6=new mango(920,200,29);
+	mango7=new mango(1050,210,30);
+	treeObj=new tree(1050,580);
+	groundObject=new ground(width/2,600,width,20);
+	stone1=new stone(240,415,30);
+	slingshot = new Slingshot(stone1.body,{x:240,y:415});
+
+
+}
+
+function draw() {
+  background(230);
+  Engine.update(engine);
+  //Add code for displaying text here!
+  image(boy,200,340,200,300);
+  groundObject.display();
+  slingshot.display();  
+  treeObj.display();
+  mango1.display();
+  mango2.display();
+  mango3.display();
+  mango4.display();
+  mango5.display();
+  mango6.display();
+  mango7.display();
+  stone1.display();
+  detectCollision(stone1,mango1);
+  detectCollision(stone1,mango2);
+  detectCollision(stone1,mango3);
+  detectCollision(stone1,mango4);
+  detectCollision(stone1,mango5);
+  detectCollision(stone1,mango6);
+  detectCollision(stone1,mango7);
+    textSize(24);
+    fill("red");
+    text("Press space to try again",10,30);      
+}
+function mouseDragged(){
+    Matter.Body.setPosition(stone1.body,{x:mouseX, y:mouseY})
+}
+function mouseReleased(){
+    slingshot.fly();
+}
+function detectCollision(lstone, lmango){
+mangoBodyPosition=lmango.body.position;
+stoneBodyPosition=lstone.body.position;
+var distance=dist(stoneBodyPosition.x,stoneBodyPosition.y,mangoBodyPosition.x,mangoBodyPosition.y)
+if(distance<=lmango.r+lstone.r){
+  Matter.Body.setStatic(lmango.body,false);
+}
+}
+function keyPressed(){
+if(keyDown("space")){
+  // stone1.x=240;
+  // stone1.y=415;
+   slingshot = new Slingshot(stone1.body,{x:240,y:415});      
+ }
+}
